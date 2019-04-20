39f9adc010b9b4a541dc496493de336ab7039056
chris pierre <chrispierrelouis.cpl@gmail.com>
2019-04-19 18:11:45 -0800

    styling today css flexbox

--- capitadoa/src/App.js
+++ capitadoa/src/App.js
@@ -19,45 +19,43 @@ API
   render() {
     return (
       <div className="capitaboards">
-      <div className="marquee">
-     
-      <marquee direction ="left" scrollamount ="10" onmouseover="this.stop();" onmouseout="this.start();">
       <ul>
         <li>
         ext of thn the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containi
         </li>
       </ul>
-      </marquee>
-      </div>
-      {/* <div>
-        <img src ={logo}></img>
-        </div> */}
-      <div className='iframe-container'>
+     
+      {/* <div className='iframe-container'>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/AxCwlnBCo88" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
-      <div className ='container'>
-  <h1>Capita Boards</h1>
-  <div className="flex-container">
-  {this.state.boards.map(board => (
-    <div>
-    <img className ='img' src = {board.imgUrl}/>
-    <div className ='card-body'>
-    <h4 className='name'>{board.boardName}</h4>
-   <p className='title'>{board.boardTitle}</p>
-      </div>
-    
-   
-      </div>
+    */}
+      <div className='mens'> 
+       <h1>MEN'S</h1>
+       </div>
+     
+  <div className = 'container-1'>
   
-  ))}
-
-
+  {this.state.boards.map(board => (
+<div className='box-1'>
+<img src = {board.imgUrl}/>
+<h4>{board.boardName}</h4>
+<p>{board.boardTitle}</p>
 </div>
-      </div>
-      </div>
-    
+))}
+</div>
+</div>
+
+
+      
+      
     );
   }
 }
 
 export default App;
+
+{/* 
+// {board.imgUrl}
+//{board.boardName}
+//{board.boardTitle}
+//{this.state.boards.map(board => () */}
