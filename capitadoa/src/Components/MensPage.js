0adcedf37367e08677124307cd9ddd415c2aff3b
chris pierre <chrispierrelouis.cpl@gmail.com>
2019-04-19 18:38:21 -0800

    almost completed with main page

---
+++ capitadoa/src/Components/MensPage.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+// import logo from './images/logo.png';
+import './App.css';
+import API from '../utils/API'
+
+class Mens extends Component {
+state = {
+  boards: []
+}
+
+componentDidMount(){
+API
+.getAllBoards()
+.then(res => this.setState({boards: res.data}))
+
+
+}
+  
+  render() {
+    return (
+      <div className="capitaboards">
+      <ul>
+        <li>
+        ext of thn the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containi
+        </li>
+      </ul>
+     
+      {/* <div className='iframe-container'>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/AxCwlnBCo88" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    */}
+      <div className='mens'> 
+       <h1>MEN'S</h1>
+       </div>
+     
+  <div className = 'container-1'>
+  
+  {this.state.boards.map(board => (
+<div className='box-1'>
+<img src = {board.imgUrl}/>
+<h4>{board.boardName}</h4>
+<p>{board.boardTitle}</p>
+</div>
+))}
+</div>
+</div>
+
+
+      
+      
+    );
+  }
+}
+
+export default Mens;
+
+{/* 
+// {board.imgUrl}
+//{board.boardName}
+//{board.boardTitle}
+//{this.state.boards.map(board => () */}
